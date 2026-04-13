619c40b6547659e9e2614a29181c81cf3b87e68d
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-13 12:02:26 +0700

    feat: add metadata in response

--- backend/src/utils/response.ts
+++ backend/src/utils/response.ts
@@ -1,16 +1,27 @@
 import { Response } from "express";
 
-export const successResponse = <T>(res: Response, data: T, message: string) => {
-  return res.status(200).json({
+export const successResponse = <T>(
+  res: Response,
+  data: T,
+  message = "Success",
+  statusCode = 200,
+) => {
+  return res.status(statusCode).json({
     success: true,
     message,
+    metadata: {
+      status: statusCode,
+    },
     data,
   });
 };
 
-export const errorResponse = (res: Response, message: string, code = 400) => {
+export const errorResponse = (res: Response, message = "Error", code = 400) => {
   return res.status(code).json({
     success: false,
     message,
+    metadata: {
+      status: code,
+    },
   });
 };
