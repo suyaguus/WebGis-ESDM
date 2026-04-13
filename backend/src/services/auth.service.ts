f7ab5de218b20d5a5a1fdbbbb3e1ff790b507c9b
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-13 11:24:39 +0700

    fix: fix code

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -2,26 +2,46 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import prisma from "../config/prisma";
 
+type Role =
+  | "super_admin"
+  | "admin_perusahaan"
+  | "kepala_instansi"
+  | "supervisor";
+
 type RegisterInput = {
   name: string;
   email: string;
   password: string;
-  role: string;
+  role: Role;
 };
 
 export const registerUser = async (data: RegisterInput) => {
+  const existingUser = await prisma.user.findUnique({
+    where: { email: data.email },
+  });
+
+  if (existingUser) {
+    throw new Error("Email sudah digunakan");
+  }
+
   const hashedPassword = await bcrypt.hash(data.password, 10);
 
-  return prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       ...data,
       password: hashedPassword,
     },
   });
+
+  const { password, ...safeUser } = user;
+
+  return safeUser;
 };
 
 export const loginUser = async (email: string, password: string) => {
-  const user = await prisma.user.findUnique({ where: { email } });
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
 
   if (!user) throw new Error("User tidak ditemukan");
 
@@ -29,10 +49,20 @@ export const loginUser = async (email: string, password: string) => {
   if (!isMatch) throw new Error("Password salah");
 
   const token = jwt.sign(
-    { id: user.id, role: user.role },
+    {
+      id: user.id,
+      role: user.role,
+    },
     process.env.JWT_SECRET!,
     { expiresIn: "1d" },
   );
 
-  return { user, token };
+  const { password: _, ...safeUser } = user;
+
+  return {
+    user: safeUser,
+    token,
+  };
 };
+
+// console.log("JWT_SECRET SIGN:", process.env.JWT_SECRET);
