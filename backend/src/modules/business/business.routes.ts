9f2c9a405585ad2d2b8feb9af3e4a083a207812c
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-15 08:44:58 +0700

    fix: fix code

--- backend/src/modules/business/business.routes.ts
+++ backend/src/modules/business/business.routes.ts
@@ -1,13 +1,43 @@
 import express from "express";
 import * as businessController from "./business.controller";
 import { authMiddleware } from "../../middleware/auth.middleware";
+import { roleMiddleware } from "../../middleware/role.middleware";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, businessController.create);
-router.get("/", authMiddleware, businessController.findAll);
+// CREATE
+router.post(
+  "/",
+  authMiddleware,
+  roleMiddleware("super_admin", "admin_perusahaan"),
+  businessController.create,
+);
+
+// GET ALL (ONLY SUPER ADMIN)
+router.get(
+  "/",
+  authMiddleware,
+  roleMiddleware("super_admin"),
+  businessController.findAll,
+);
+
+// GET BY ID
 router.get("/:id", authMiddleware, businessController.findOne);
-router.patch("/:id", authMiddleware, businessController.update);
-router.delete("/:id", authMiddleware, businessController.remove);
+
+// UPDATE
+router.patch(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("super_admin", "admin_perusahaan"),
+  businessController.update,
+);
+
+// DELETE (ONLY SUPER ADMIN)
+router.delete(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("super_admin"),
+  businessController.remove,
+);
 
 export default router;
