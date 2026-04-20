5abbe0230b04672c58ce564434f8772572e85792
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-20 10:01:02 +0700

    feat: fix routes for well

--- backend/src/modules/well/well.routes.ts
+++ backend/src/modules/well/well.routes.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post(
   "/",
   authMiddleware,
-  roleMiddleware("super_admin", "admin_perusahaan"),
+  roleMiddleware("super_admin", "admin_perusahaan", "supervisor"),
   wellController.create,
 );
 
