a1e95323e6cee5802385ca1ccc732388062215a1
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-04-21 16:02:11 +0700

    testing deploy

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import * as authController from "../controllers/auth.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.get("/me", authMiddleware, authController.getMe);
 
 export default router;
